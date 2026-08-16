e18526b339100e6345d4b37f92ba1a8b3d9dde5c
Arnab Maity <arnabm776@gmail.com>
2026-08-16 10:51:23 +0530

    Fix Tailwind v4 custom dark variant directive and light mode toggle styling

--- src/layouts/HomeLayout.tsx
+++ src/layouts/HomeLayout.tsx
@@ -5,7 +5,7 @@ type Props = {};
 
 const HomeLayout = ({}: Props) => {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col bg-white dark:bg-[#09090b] text-gray-900 dark:text-gray-100">
       <Navbar />
 
       <main className="flex-grow p-6">
